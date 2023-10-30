9cc1604285143b241307eecacf844cd69a309c04
Samuel Stacey <ss3467@bath.ac.uk>
2023-10-30 13:48:16 +0000

    Change to run pipeline

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -14,6 +14,7 @@ public class App
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
         System.out.println(sayHelloToSomeone("Samuel Stacey"));
+        System.out.println(sayHelloToSomeone("Test"));
         System.out.println(sayGoodbye());
     }
 
